74e94612e0d30ec22114501b9dea60c9db3b552f
Peter Parada <parad.peter@gmail.com>
2017-02-17 14:12:24 +0100

    Update bob.clj

--- bob.clj
+++ bob.clj
@@ -2,10 +2,6 @@
 
 (require '[clojure.string :as str])
 
-(defn only-alpha [text]
-  (apply str (filter #(Character/isLetter %) text))
-)
-
 (defn is-upper? [text]
   (and 
    (not (some #(Character/isLowerCase %) text)) 
