a09d96d63783f86e3aca1b4fbea99fdac9eddd9d
Peter Parada <parad.peter@gmail.com>
2017-09-16 12:45:11 +0200

    Update bob.clj

--- bob.clj
+++ bob.clj
@@ -5,25 +5,20 @@
 (defn is-upper? [text]
   (and 
    (not (some #(Character/isLowerCase %) text)) 
-   (some #(Character/isUpperCase %) text))
-)
+   (some #(Character/isUpperCase %) text)))
 
 (defn nothing? [sentence]
-  (empty? (str/trim sentence))
-)
+  (empty? (str/trim sentence)))
 
 (defn yell? [sentence]
-  (is-upper? sentence)
-)
+  (is-upper? sentence))
 
 (defn question? [sentence]
-  (= (last sentence) \?)
-)
+  (= (last sentence) \?))
 
 (defn response-for [sentence]
   (cond
     (nothing? sentence) "Fine. Be that way!"
     (yell? sentence) "Whoa, chill out!"
     (question? sentence) "Sure."
-    :else "Whatever.")
-)
+    :else "Whatever."))
